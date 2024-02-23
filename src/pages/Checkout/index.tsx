1b4f815e5c78981d12e47785a7d9f4977d212696
Maria Cidoncha <mariacidoncha@gmail.com>
2024-02-23 10:34:43 +0100

    add checkout options and delivery

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -6,6 +6,8 @@ import { NavBar } from '../../components/layouts/NavBar';
 import { useAuthContext } from '../../context/AuthContext';
 import { useProductContext } from '../../context/ProductContext';
 import { ChangeEvent, useEffect, useState } from 'react';
+import { discounts } from '../../assets/data/discounts';
+import toast, { Toaster } from 'react-hot-toast';
 
 export interface ICheckoutProps {}
 
@@ -19,18 +21,21 @@ export function Checkout() {
       0
     ) ?? 0;
   const [discount, setDiscount] = useState(0);
+  const [discountInput, setDiscountInput] = useState('');
   const [shipping, setShipping] = useState(10.5);
   const [total, setTotal] = useState(0);
+  const [paymentClass, setPaymentClass] = useState('add-card hide');
+  const [errorClass, setErrorClass] = useState('hide');
 
   function totalCost() {
-    let total = productsPrice - discount + shipping;
+    let total = productsPrice + discount + shipping;
 
     setTotal(parseFloat(total.toFixed(2)));
   }
 
   useEffect(() => {
     totalCost();
-  }, [shipping]);
+  }, [shipping, discount]);
 
   function handleClick() {
     navigate(-1);
@@ -40,26 +45,70 @@ export function Checkout() {
     setShipping(parseFloat(e.target.id));
   }
 
+  function handleChangeCard(e: ChangeEvent<HTMLInputElement>) {
+    if (e.target.checked) {
+      setPaymentClass('add-card');
+    } else {
+      setPaymentClass('add-card hide');
+    }
+  }
+
+  function handleClickCoupon() {
+    const discountFound = discounts.find((d) => {
+      return d.name === discountInput;
+    });
+    if (discountFound) {
+      setErrorClass('hide');
+      setDiscount(
+        -parseFloat((productsPrice * discountFound.value).toFixed(2))
+      );
+      toast.success('Successfully toasted!', {
+        style: {
+          fontSize: '1.5rem',
+        },
+      });
+    } else {
+      setErrorClass('');
+      setDiscount(0);
+      toast.error('Invalid coupon', {
+        style: {
+          fontSize: '1.5rem',
+        },
+      });
+    }
+  }
+
+  function handleChangeDiscountValue(e: ChangeEvent<HTMLInputElement>) {
+    setDiscountInput(e.target.value);
+  }
+
   return (
     <>
+      <Toaster />
       <BsArrowLeftCircle onClick={handleClick} className="back" />
       <section className="title-section">
         <IoCartOutline className="icon" />
         <h2>Cart check out</h2>
       </section>
       <section className="accordion-checkout">
-        <details className="details-checkout" name="checkout" open>
+        <details name="checkout" open>
           <summary>Resume</summary>
           <ul>
             {user.user?.cart.map((e) => {
               const product = products.products.find((el) => {
                 return e.id.toString() === el.id;
               });
+              const coverOption = product?.options.find((o) => {
+                return o.price === e.option;
+              })?.cover;
               return (
                 <li key={`${e.id}p${e.option}`}>
                   <img src={product?.image} alt={`${product?.name} cover`} />
                   <section className="product-details">
-                    <p>{product?.name}</p>
+                    <p>
+                      {product?.name}{' '}
+                      <span className="book-option">({coverOption})</span>
+                    </p>
                     <p>{e.quantity} items</p>
                     <p>{e.option * e.quantity} €</p>
                   </section>
@@ -68,47 +117,93 @@ export function Checkout() {
             })}
           </ul>
         </details>
-        <details className="details-checkout" name="checkout">
+        <details name="checkout">
           <summary>Shipping</summary>
           <ul>
             <li>
-              <label>
+              <label className="card-section-label">
                 <input
                   id="10.5"
                   onChange={handleChange}
+                  className="add-card-input"
                   name="shipping"
                   type="radio"
                   defaultChecked
                 />
-                Fast delivery
+                Fast delivery (10.50 €)
               </label>
             </li>
             <li>
-              <label>
+              <label className="card-section-label">
                 <input
                   id="2.3"
                   onChange={handleChange}
+                  className="add-card-input"
                   name="shipping"
                   type="radio"
                 />
-                Normal delivery
+                Normal delivery (2.30 €)
               </label>
             </li>
             <li>
-              <label>
+              <label className="card-section-label">
                 <input
                   id="0"
                   onChange={handleChange}
+                  className="add-card-input"
                   name="shipping"
                   type="radio"
                 />
-                Pick up delivery
+                Pick up delivery (FREE)
               </label>
             </li>
           </ul>
         </details>
-        <details className="details-checkout" name="checkout">
+        <details name="checkout">
           <summary>Payment</summary>
+          <section className="card-section">
+            <label className="card-section-label">
+              <input
+                className="add-card-input"
+                onChange={handleChangeCard}
+                type="checkbox"
+              />
+              <img src="public/mastercard-svgrepo-com.svg" alt="" />
+              Add MasterCard
+            </label>
+            <section className={paymentClass}>
+              <label className="card-number">
+                Card number:
+                <input type="number" />
+              </label>
+              <label className="card-expiry">
+                Expiry:
+                <input type="month" />
+              </label>
+              <label className="card-code">
+                Card code:
+                <input type="number" placeholder="CVC" />
+              </label>
+            </section>
+            <label className="card-section-label">
+              <input className="add-card-input" type="checkbox" />
+              <img src="public/paypal-svgrepo-com.svg" alt="" />
+              PayPal
+            </label>
+            <label className="card-section-label">
+              <img src="public/label-tag-price-svgrepo-com.svg" alt="" />
+              Add a coupon:
+              <input
+                type="text"
+                value={discountInput}
+                onChange={handleChangeDiscountValue}
+              />
+              <small className={errorClass}>Invalid coupon</small>
+              <button onClick={handleClickCoupon} className="coupon-btn">
+                Apply
+              </button>
+            </label>
+          </section>
         </details>
       </section>
       <section className="cart-resume">
